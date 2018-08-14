d75f96e7211b157dac20c7e6822b563539ea1dbf
JeongJuhyeon <j.h.jeong@outlook.com>
2018-08-14 17:02:02 +0900

    Update to latest version at BNK (Mon Aug 13)

---
+++ src/cameraconfigtest.py
@@ -0,0 +1,13 @@
+from config import cameraConfig
+
+a = "../res/a/b/c"
+print(a.split('.')[0])
+"""d = cameraConfig()
+print(d.get_camera_number('LEFT'))
+a = cameraConfig()
+a.set_camera_number('LEFT', 1)
+print(a.get_camera_number('LEFT'))
+print(d.get_camera_number('LEFT'))
+c = cameraConfig()
+print(c.get_camera_number('LEFT'))1
+1"""
